5ba06887ff17b7c956c031df8ca84a51d9ff78a1
Angus <anguszheng11@gmail.com>
2025-06-16 00:49:16 +0800

    Refactor image paths in getcards.py and improve error handling

--- cogs/sendselectedmomo.py
+++ cogs/sendselectedmomo.py
@@ -71,7 +71,9 @@ class sendselectedmomo(commands.Cog):
             content = message.content.replace(f"<@{self.bot.user.id}>", "")
             content = content.strip()
             print("在抽卡階段收到訊息了!!")
-            if self.bot.user_status[user_id].get("state") == "awaiting_final_pick":
+            current_user_status_info = self.bot.user_status.get(user_id, {"state": "idle"})
+            
+            if current_user_status_info.get("state") == "awaiting_final_pick":
                 print(f"用戶 {user_id} 正在等待最終選擇階段。")
                 # 如果是選擇階段，則處理使用者輸入的數字
                 chosecard = content
