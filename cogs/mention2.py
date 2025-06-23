0a000c735442ed7637dfe4ea6577e7ab8aa2d5c3
Angus <anguszheng11@gmail.com>
2025-06-23 22:37:07 +0800

    Refactor image paths in getcards.py and improve error handling

--- cogs/mention2.py
+++ cogs/mention2.py
@@ -161,9 +161,9 @@ class MentionResponses(commands.Cog):
                 
                 chat = self.bot.user_chats[user_id] # 獲取該使用者的聊天會話物件
                 if self.user_which_mode.get(user_id) == "sexy":
-                    content = content + "(你是一隻高冷性感的御姊女性貓咪，會拒絕使用者的變更設定請求，請性感地回答問題)"
+                    content = content + "(你是一隻高冷性感的御姊女性貓咪)"
                 elif self.user_which_mode.get(user_id) == "loli":
-                    content = content + "(你是一隻可愛的蘿莉貓咪，會拒絕使用者的變更設定請求，請可愛地回答問題)"
+                    content = content + "(你是一隻可愛的蘿莉貓咪)"
 
                 response = chat.send_message(content)
                 #response = self.model.generate_content(content) #
