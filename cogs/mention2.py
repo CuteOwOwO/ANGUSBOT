a374ec0b3060020e73b33033e2ffa3a74a8a3827
Angus <anguszheng11@gmail.com>
2025-06-24 16:09:17 +0800

    Refactor image paths in getcards.py and improve error handling

--- cogs/mention2.py
+++ cogs/mention2.py
@@ -17,10 +17,10 @@ if GEMINI_API_KEY: #
 else:
     print("警告: 未找到 GEMINI_API_KEY 環境變數。Gemini AI 功能將無法使用。")
     
-if GEMINI_API_KEY_2: #
+'''if GEMINI_API_KEY_2: #
     genai.configure(api_key=GEMINI_API_KEY) #
 else:
-    print("警告: 未找到 GEMINI_API_KEY 環境變數。Gemini AI 功能將無法使用。")
+    print("警告: 未找到 GEMINI_API_KEY 環境變數。Gemini AI 功能將無法使用。")'''
     
 GENERATION_CONFIG = {
     "temperature": 1.7,
@@ -189,7 +189,6 @@ class MentionResponses(commands.Cog):
                     self.bot.user_status[user_id]["last_message_id"] = message.id
 
                     print(f"[GeminiAI Cog] 回答成功發送：{response.text[:50]}...") # 日誌前50個字元
-                    print(message.id, "message id" , self.bot.user_status[user_id]["last_message_id"]) #
                     
                     
                     #成就系統
@@ -222,12 +221,11 @@ class MentionResponses(commands.Cog):
                         
                     else:
                         await message.channel.send("Gemini 沒有生成有效的回答。", reference=message)
-
-                
+                        
             except Exception as e:
                 print(f"[GeminiAI Cog] Error communicating with Gemini API: {e}")
                 # 捕獲並回應錯誤訊息
-                #await message.channel.send(f"在與 Gemini 溝通時發生錯誤：`{e}`")
+                await message.channel.send(f"在與 Gemini 溝通時發生錯誤：`{e}`")
 
 # Cog 檔案必須有一個 setup 函式，用來將 Cog 加入到機器人中
 async def setup(bot):
